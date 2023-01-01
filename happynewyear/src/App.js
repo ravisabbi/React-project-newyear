3b6d335f34b8dfe01f6894e233503c3767ba5f62
ravisabbi <ravisabbi7036@gmail.com>
2023-01-01 15:50:21 +0530

    happy new year

---
+++ happynewyear/src/App.js
@@ -0,0 +1,20 @@
+ 
+import './App.css';
+
+function App() {
+  return (
+    <div className="greeting-card-container">
+    <div className="text-container">
+        <p className="greet">From: Ravi Sabbi</p>
+        
+    </div>
+    <div className="greet-text-container">
+        <h1 className="heading">Happy <br /><span class="new-year">New Year</span></h1>
+        <p className="greet-text mt-4">Wishing that the new year will bring joy, love, peace, and happiness to you.</p>
+    </div>
+</div>
+
+  );
+}
+
+export default App;
